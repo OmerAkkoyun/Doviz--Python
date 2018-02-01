c0f3734d45689fde22b4464ebe553497de72ec02
Ömer Akkoyun <33864154+OmerAkkoyun@users.noreply.github.com>
2018-02-01 20:20:23 +0300

    JSON

--- Doviz-Uygulamasi.py
+++ Doviz-Uygulamasi.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import time
+import sys
 print("""
 ----------------
 DÖVİZ UYGULAMASI
@@ -11,21 +12,23 @@ EURO        için :EUR
 TÜRK LİRASI için :TRY
 ----------------
 """)
+try:
+    paraadi=input("Para Birinizin Adı :")
+    miktar=float(input("Miktar :"))
+    cevir=input("Çevirilecek Para Birimi :")
+    print("Güncel Değerler Kontrol ediliyor..")
+    time.sleep(2)
+    print("Hesaplanıyor..")
+    time.sleep(1)
 
-paraadi=input("Para Birinizin Adı :")
-miktar=float(input("Miktar :"))
-cevir=input("Çevirilecek Para Birimi :")
-print("Güncel Değerler Kontrol ediliyor..")
-time.sleep(2)
-print("Hesaplanıyor...")
-time.sleep(1)
+    url="https://api.fixer.io/latest?base="
+    getir=requests.get(url+paraadi)
+    json_et=getir.json()
 
-url="https://api.fixer.io/latest?base="
-getir=requests.get(url+paraadi)
-json_et=getir.json()
-print("Sonuç =  ",(json_et["rates"][cevir])*miktar,cevir)
-
-
+    print("Sonuç =  ",(json_et["rates"][cevir])*miktar,cevir)
+except:
+    sys.stderr.write("Geçerli para birimi girin!")
+    sys.stderr.flush()
 
 
 
